8c664df9368ab7c6674959909d58ceab81c0e01a
Ertunga Yusuf OCAK <ocakertungayusuf@gmail.com>
2024-12-16 18:45:25 +0300

    Login Ekranı Frontend

---
+++ src/main/java/org/rent/arackiralamasistemi/Controller.java
@@ -0,0 +1,38 @@
+package org.rent.arackiralamasistemi;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class Controller {
+
+    @FXML
+    private Label createAccountLink;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private Label wrongCredentialsLabel;
+
+    @FXML
+    void onCreateAccount(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onLoginButtonClick(ActionEvent event) {
+
+    }
+
+}
