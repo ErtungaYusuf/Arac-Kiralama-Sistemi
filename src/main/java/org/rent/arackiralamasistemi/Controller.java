79a8c2edacf563909ee96c32405a0086e6c9c869
helyaKi <itsmehelya@gmail.com>
2024-12-18 11:09:19 +0300

    Revert "Arabalar Menusu Frontend"
    This reverts commit f76477ee5fcf9caf4d441361a46103b4954dee62.

--- src/main/java/org/rent/arackiralamasistemi/Controller.java
+++ src/main/java/org/rent/arackiralamasistemi/Controller.java
@@ -2,25 +2,13 @@ package org.rent.arackiralamasistemi;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-import java.util.Objects;
 
 public class Controller {
-
-    private Parent root;
-    private Stage stage;
-    private Scene scene;
 
     @FXML
     private Label createAccountLink;
@@ -31,39 +19,20 @@ public class Controller {
     @FXML
     private PasswordField password;
 
-
     @FXML
     private TextField username;
 
-    // hatalı login işleminde değiştirilmek için atanmış label:
     @FXML
     private Label wrongCredentialsLabel;
 
-    // Login ekranı, hesap oluştur butonu tıklandığında:
     @FXML
     void onCreateAccount(MouseEvent event) {
 
     }
 
-    // Login ekranı giriş yap butonu tıklandığında:
     @FXML
     void onLoginButtonClick(ActionEvent event) {
-           //databaseden çekme ve kontrol etme işlemi
-           // şimdilik doğrudan geçiş izni verilmiş durumda:
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        changeScene(stage, "cars-menu");
-    }
 
-    private void changeScene(Stage stage, String fxmlName) {
-        try {
-            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(fxmlName + ".fxml")));
-            scene = new Scene(root);
-            stage.setTitle("Araç Kiralama Sistemi");
-            stage.setScene(scene);
-            stage.show();
-        } catch (IOException e) {
-            System.out.println("Hata Giriş Yapılamıyor.");
-        }
     }
 
 }
