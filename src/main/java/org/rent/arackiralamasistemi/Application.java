da22e449af5fb8a61bf12e0d822decf4cce51fce
Ertunga Yusuf OCAK <ocakertungayusuf@gmail.com>
2024-12-18 11:44:01 +0300

    b

--- src/main/java/org/rent/arackiralamasistemi/Application.java
+++ src/main/java/org/rent/arackiralamasistemi/Application.java
@@ -14,6 +14,7 @@ public class Application extends javafx.application.Application {
         stage.setTitle("Araç Kiralama Sistemi");
         stage.setScene(scene);
         stage.show();
+        System.out.println("deneme");
     }
 
     public static void main(String[] args) {
