e0ea7547749de9fd47301ac1253fdcddfcb7082d
Ertunga Yusuf OCAK <ocakertungayusuf@gmail.com>
2024-12-18 11:20:15 +0300

    deneme2

--- src/main/java/org/rent/arackiralamasistemi/Controller.java
+++ src/main/java/org/rent/arackiralamasistemi/Controller.java
@@ -2,13 +2,25 @@ package org.rent.arackiralamasistemi;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class Controller {
+
+    private Parent root;
+    private Stage stage;
+    private Scene scene;
+
 
     @FXML
     private Label createAccountLink;
@@ -33,6 +45,18 @@ public class Controller {
     @FXML
     void onLoginButtonClick(ActionEvent event) {
 
+    }
+
+    private void changeScene(Stage stage, String fxmlName) {
+        try {
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(fxmlName + ".fxml")));
+            scene = new Scene(root);
+            stage.setTitle("Library Management");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            System.out.println("Hata Giriş Yapılamıyor.");
+        }
     }
 
 }
