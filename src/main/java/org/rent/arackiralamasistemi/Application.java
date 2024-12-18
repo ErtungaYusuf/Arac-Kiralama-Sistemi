38d81223f4794fc5c45f7856d7b1092143c63c90
helyaKi <itsmehelya@gmail.com>
2024-12-18 11:08:02 +0300

    den

--- src/main/java/org/rent/arackiralamasistemi/Application.java
+++ src/main/java/org/rent/arackiralamasistemi/Application.java
@@ -14,6 +14,7 @@ public class Application extends javafx.application.Application {
         stage.setTitle("Araç Kiralama Sistemi");
         stage.setScene(scene);
         stage.show();
+        //fkjukmy
     }
 
     public static void main(String[] args) {
